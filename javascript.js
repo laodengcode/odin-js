931330113b8f400a452668091a3f87d84813cb86
laodeng <93652888+laodengcode@users.noreply.github.com>
2025-01-08 21:21:41 +0100

    basic js

---
+++ javascript.js
@@ -0,0 +1,39 @@
+let name = "john";
+let surname = "doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+
+const pi = 3.14;
+
+console.log(pi);
+
+let result = (3 + 2) - 76 * (1 + 1);
+console.log(result);
+
+
+console.log(11 + 2);
+
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+9 * a;
+
+let b = 7 * a;
+console.log(b);
+
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+
+
